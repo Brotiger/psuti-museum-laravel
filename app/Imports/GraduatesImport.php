b525bde333d8cb3bd03b5df54b30d6d8ed60b21c
Brotiger <dimka@bdima.ru>
2021-04-27 11:45:00 +0400

    fix add file

--- app/Imports/GraduatesImport.php
+++ app/Imports/GraduatesImport.php
@@ -31,7 +31,7 @@ class GraduatesImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         $registrationExist = (!empty($row['Регистрационный номер']) && Graduate::where('registrationNumber', $row['Регистрационный номер'])->get()->count()) ? true : false;
-        if(!empty($row['Серия документа']) && !empty($row['Номер документа']) && !empty($row['Фамилия получателя']) && !empty($row['Имя получателя']) && !$registrationExist){
+        if((!empty($row['Фамилия получателя']) || !empty($row['Фамилия'])) && (!empty($row['Имя получателя']) || !empty($row['Имя'])) && !$registrationExist){
 
             $confirmDelete = null;
 
@@ -45,28 +45,44 @@ class GraduatesImport implements ToModel, WithHeadingRow
                 $row['Высшее образование, получаемое впервые'] == 'Да'? true : false;
             }
 
+            if(!empty($row['Отчество получателя'])){
+                $secondName = $row['Отчество получателя'];
+            }else if (!empty($row['Отчество'])){
+                $secondName = $row['Отчество'];
+            }else{
+                $secondName = null;
+            }
+
+            if(!empty($row['Код специальности, направления подготовки'])){
+                $specialtyCode = $row['Код специальности, направления подготовки'];
+            }else if(!empty($row['Код специальности по направлению'])){
+                $specialtyCode = $row['Код специальности по направлению'];
+            }else{
+                $specialtyCode = $row['Код специальности по справочнику'];
+            }
+
             return new Graduate([
                 'addUserId' => Auth::user()->id,
                 'documentName' => !empty($row['Наименование документа'])? $row['Наименование документа'] : $row['Название документа'],
                 'documentType' => $row['Вид документа'],
-                'documentStatus' => $row['Статус документа'],
+                'documentStatus' => !empty($row['Статус документа'])? $row['Статус документа'] : null,
                 'confirmLoss' => $row['Подтверждение утраты'] == 'Да'? true : false,
                 'confirmSwap' => $row['Подтверждение обмена'] == 'Да'? true : false,
                 'confirmDelete' => $confirmDelete,
-                'educationLevel' => $row['Уровень образования'],
-                'series' => $row['Серия документа'],
-                'number' => $row['Номер документа'],
+                'educationLevel' => !empty($row['Уровень образования'])? $row['Уровень образования'] : null,
+                'series' => !empty($row['Серия документа'])? $row['Серия документа'] : null,
+                'number' => !empty($row['Номер документа'])? $row['Номер документа'] : null,
                 'issueDate' => date('Y-m-d', ((int) $row['Дата выдачи'] - 25569) * 86400),
                 'registrationNumber' => $row['Регистрационный номер'],
-                'specialtyCode' => $row['Код специальности, направления подготовки'],
-                'specialtyName' => $row['Наименование специальности, направления подготовки'],
-                'qualificationName' => $row['Наименование квалификации'],
+                'specialtyCode' => $specialtyCode,
+                'specialtyName' => !empty($row['Наименование специальности, направления подготовки'])? $row['Наименование специальности, направления подготовки'] : $row['Направление подготовки/Специальность'],
+                'qualificationName' => !empty($row['Наименование квалификации'])? $row['Наименование квалификации'] : $row['Квалификация/Степень'], //$row['Наименование квалификации'],//!empty($row['Наименование квалификации'])? $row['Наименование квалификации'] : $row['Квалификация/Степень'],
                 'enteredYear' => $row['Год поступления'],
                 'exitYear' => $row['Год окончания'],
                 'trainingPeriod' => (int) $row['Год окончания'] - (int) $row['Год поступления'],
-                'lastName' => $row['Фамилия получателя'],
-                'firstName' => $row['Имя получателя'],
-                'secondName' => $row['Отчество получателя'],
+                'lastName' => !empty($row['Фамилия получателя'])? $row['Фамилия получателя'] : $row['Фамилия'],
+                'firstName' => !empty($row['Имя получателя'])? $row['Имя получателя'] : $row['Имя'],
+                'secondName' => $secondName,
                 'dateBirthday' => date('Y-m-d', ((int) $row['Дата рождения получателя'] - 25569) * 86400),
                 'sex' => $row['Пол получателя'],
                 'citizenship' => !empty($row['Гражданство получателя (код страны по ОКСМ)'])? $row['Гражданство получателя (код страны по ОКСМ)'] : null,
